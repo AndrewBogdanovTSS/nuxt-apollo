a6f3333f78b9acb4af2bb2093a525837c8143428
Andrew <andrewbogdanov@ukr.net>
2018-07-16 13:38:54 +0300

    initial project setup

---
+++ nuxt.config.js
@@ -0,0 +1,10 @@
+module.exports =  {
+  modules: ['@nuxtjs/apollo'],
+  apollo: {
+    clientConfigs: {
+      default: {
+        httpEndpoint: 'https://api.graph.cool/simple/v1/cj1dqiyvqqnmj0113yuqamkuu'
+      }
+    }
+  }
+}
